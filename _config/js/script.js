7ac549fc8039b19e6427f35c0ad791214e3d391e
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-10-20 15:22:17 -0300

    update

---
+++ _config/js/script.js
@@ -0,0 +1,18 @@
+var slideTitle = {
+    reset: true,
+    distance: '150%',
+    origin: 'left',
+    duration: 1000,
+    opacity: 0
+};
+
+var slideContent = {
+    reset: true,
+    distance: '150%',
+    origin: 'left',
+    duration: 1550,
+    opacity: 0
+};
+
+ScrollReveal().reveal('.title-content', slideTitle);
+ScrollReveal().reveal('.conte-content', slideContent);
